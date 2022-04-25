5cc38aa2a608b9ac083ce15547a744454e5d4f20
piwi3 <99548889+piwi3@users.noreply.github.com>
2022-04-25 16:08:32 +0200

    added credentials_tbd.py file

---
+++ code/my_twitter_app/streamlit_app/credentials_tbd.py
@@ -0,0 +1,15 @@
+# Please rename this file to credentials.py and add your personal information
+
+# Twitter API
+
+API_KEY = 
+API_KEY_SECRET = 
+BEARER_TOKEN = 
+ACCESS_TOKEN = 
+ACCESS_TOKEN_SECRET = 
+
+# PostgreSQL
+
+POSTGRES_USER = 
+POSTGRES_PASSWORD = 
+POSTGRES_DB = 
